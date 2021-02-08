4b326238db6a95cd0b5e1fcf7d8ddb8b9d5ad673
Aleksander <msxander@gmail.com>
2021-02-08 12:40:09 +0300

    Remove debug code

--- src/main/java/ru/otus/jdbcprj/Main.java
+++ src/main/java/ru/otus/jdbcprj/Main.java
@@ -3,8 +3,6 @@ package ru.otus.jdbcprj;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import ru.otus.jdbcprj.dao.GenreDao;
-import ru.otus.jdbcprj.model.Genre;
 
 @SpringBootApplication
 public class Main {
@@ -12,15 +10,6 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         ApplicationContext context = SpringApplication.run(Main.class);
-
-        GenreDao genreDao = context.getBean(GenreDao.class);
-
-        Genre genre = genreDao.getById(1);
-
-        System.out.println(genre);
-
-//        genreDao.getAll().stream().forEach(System.out::println);
-        genreDao.getAll().stream().forEach(System.out::println);
     }
 
 }
